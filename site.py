875cc4325d6e785efaa7a03e9e25f78b6ffa78d9
Kevin Gangi <kevin@gangi-home.us>
2025-02-07 01:42:01 -0500

    First release of this website

---
+++ site.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import sys
+import yaml
+import pdfkit # type: ignore
+from jinja2 import Environment, FileSystemLoader
+from math import floor
+from os import listdir
+
+env = Environment(
+    loader=FileSystemLoader('./'),
+)
+
+template = env.get_template('site.html.j2')
+if len(sys.argv) > 1:
+    site_yaml = sys.argv[1]
+else:
+    site_yaml = "site.yaml"
+
+cssFiles = []
+for css in listdir("./styles"):
+    cssFiles.append("./styles/" + css)
+
+with open(site_yaml, 'r') as file:
+    site = yaml.load(file, Loader=yaml.FullLoader)
+
+    name = site["name"]
+    title = site["title"]
+    info = site["info"]
+    skills = site["skills"]
+    intro = site["intro"]
+    experience = site["experience"]
+    education = site["education"]
+    certifications = site["certifications"]
+    honors = site["honors"]
+    projects = site["projects"]
+
+    html = template.render(name = name,
+                           title = title,
+                           info = info,
+                           skills = skills,
+                           intro = intro,
+                           experience = experience,
+                           education = education,
+                           cssFiles = cssFiles,
+                           certifications = certifications,
+                           honors = honors,
+                           projects = projects)
+
+    options = {
+      "enable-local-file-access": None,
+      "page-size": "Letter",
+      "margin-top": "2",
+      "margin-right": "2",
+      "margin-bottom": "2",
+      "margin-left": "2",
+      "encoding": "UTF-8"
+    }
+
+    with open('index.html', 'w') as f:
+        f.write(html)
+
+    # pdfkit.from_string(html, "site.pdf", options=options, css=cssFiles)
